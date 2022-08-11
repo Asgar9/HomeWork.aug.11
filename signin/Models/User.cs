b39cc69068185062d12759967fe1a559e2ff63c0
Asgar9 <asgaryas@code.edu.az>
2022-08-11 11:24:30 +0400

    Registration form eample

---
+++ signin/Models/User.cs
@@ -0,0 +1,56 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace signin.Models
+{
+    class User
+    {
+        public string UserName
+        {
+            get 
+            {
+                return UserName;
+            }
+            set
+            {
+                if (value.Length>5 && value.Length<26)
+                 Console.WriteLine(value);
+                else
+                  Console.WriteLine("Wrong Username type");
+            } 
+        }
+        public string Password 
+        { 
+            get  
+            {
+                return Password;
+            }
+            set
+            {
+                if (value.Length > 7 && value.Length < 26 && value.Any(char.IsDigit) && value.Any(char.IsLower)&& value.Any(char.IsUpper))
+                    Console.WriteLine(value);
+                else
+                    Console.WriteLine("Wrong password type");
+            }
+        }
+        public User(string UserName)
+        {
+            this.UserName = UserName;
+        }
+        //public bool HasDigit(string word)
+        //{
+        //    if (word.Any(char.IsDigit))
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        
+    }
+}
+
+
+            
+               
+                
---
+++ signin/Program.cs
@@ -0,0 +1,17 @@
+﻿using signin.Models;
+using System;
+
+namespace signin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the username:");
+            User admin = new User(Console.ReadLine());
+            Console.WriteLine("Enter the password");
+            admin.Password=Console.ReadLine();
+            
+        }
+    }
+}
